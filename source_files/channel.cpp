93a65348cea92d043ed7caf4520d23a610413d7e
shamzaou <shamzaou@student.42abudhabi.ae>
2024-07-08 11:38:05 +0400

    git error

--- source_files/channel.cpp
+++ source_files/channel.cpp
@@ -1,120 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   channel.cpp                                        :+:      :+:    :+:   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shamzaou <shamzaou@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/23 17:24:24 by shamzaou          #+#    #+#             */
-/*   Updated: 2024/07/07 09:25:25 by shamzaou         ###   ########.fr       */
+/*   Created: 2024/06/08 11:50:49 by alsaeed           #+#    #+#             */
+/*   Updated: 2024/06/13 17:47:33 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Channel.hpp"
-#include "../includes/Client.hpp"
+#include <Channel.hpp>
 
-Channel::Channel(const std::string& name)
-    : _name(name), _topic(""), _key(""), _inviteOnly(false),
-      _topicRestricted(false), _userLimit(0) {}
+void	Channel::addClient( int client_fd ) {
 
-Channel::~Channel() {
-    // Cleanup code
+	_clients.insert( client_fd );
+
+	return;
 }
 
-void Channel::addClient(Client* client) {
-    _clients[client->getFd()] = client;
+void	Channel::removeClient( int client_fd ) {
+
+	_clients.erase( client_fd );
+
+	return;
 }
 
-void Channel::removeClient(Client* client) {
-    _clients.erase(client->getFd());
-    _operators.erase(client->getFd());
-}
+void	Channel::broadcastMessage( const std::string &message, int sender_fd ) {
 
-void Channel::broadcastMessage(const std::string& message, Client* sender) {
-    for (std::map<int, Client*>::const_iterator it = _clients.begin(); it != _clients.end(); ++it) {
-        if (it->second != sender) {
-            it->second->sendMessage(message);
-        }
-    }
-}
+	for ( std::set<int>::iterator it = _clients.begin(); it != _clients.end(); ++it ) {
 
-bool Channel::isClientInChannel(Client* client) const {
-    return _clients.find(client->getFd()) != _clients.end();
-}
+		int client_fd = *it;
+		if ( client_fd != sender_fd ) {
 
-bool Channel::isOperator(Client* client) const {
-    std::map<int, bool>::const_iterator it = _operators.find(client->getFd());
-    return it != _operators.end() && it->second;
-}
+			if ( send( client_fd, message.c_str(), message.length(), 0 ) == -1 ) {
 
-void Channel::setTopic(const std::string& topic, Client* client) {
-    if (!_topicRestricted || isOperator(client)) {
-        _topic = topic;
-    }
-}
-
-void Channel::addOperator(Client* client) {
-    _operators[client->getFd()] = true;
-}
-
-void Channel::removeOperator(Client* client) {
-    _operators.erase(client->getFd());
-}
-
-void Channel::kickClient(Client* client, Client* kickedBy, const std::string& reason) {
-    if (isOperator(kickedBy)) {
-        removeClient(client);
-        client->sendMessage("You have been kicked from " + _name + " by " + kickedBy->getNickname() + " (" + reason + ")");
-    }
-}
-
-void Channel::setKey(const std::string& key, Client* client) {
-    if (isOperator(client)) {
-        _key = key;
-    }
-}
-
-void Channel::setInviteOnly(bool inviteOnly, Client* client) {
-    if (isOperator(client)) {
-        _inviteOnly = inviteOnly;
-    }
-}
-
-void Channel::setTopicRestricted(bool topicRestricted, Client* client) {
-    if (isOperator(client)) {
-        _topicRestricted = topicRestricted;
-    }
-}
-
-void Channel::setUserLimit(int userLimit, Client* client) {
-    if (isOperator(client)) {
-        _userLimit = userLimit;
-    }
-}
-
-void Channel::inviteClient(Client* client, Client* invitedBy) {
-    if (_inviteOnly && isOperator(invitedBy)) {
-        addClient(client);
-        client->sendMessage("You have been invited to " + _name + " by " + invitedBy->getNickname());
-    }
+				throw IrcException( "Can't send message to client" );
+			}
 		}
+	}
 
-const std::string& Channel::getName() const {
-    return _name;
+	return;
 }
 
-const std::string& Channel::getTopic() const {
-    return _topic;
-}
-
-bool Channel::isInviteOnly() const {
-    return _inviteOnly;
-}
-
-bool Channel::isTopicRestricted() const {
-    return _topicRestricted;
-}
-
-int Channel::getUserLimit() const {
-    return _userLimit;
-}
+// Path: includes/Channel.hpp
