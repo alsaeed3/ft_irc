31aee2566b9dd07152b68ebc9879c97abf87d74a
alsaeed3 <ali.eissa.570@gmail.com>
2024-07-12 18:58:54 +0400

    fixed cap

--- source_files/Commands.cpp
+++ source_files/Commands.cpp
@@ -6,7 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 07:48:18 by alsaeed           #+#    #+#             */
-/*   Updated: 2024/07/12 18:20:30 by alsaeed          ###   ########.fr       */
+/*   Updated: 2024/07/12 18:58:43 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void Server::connectUser(Client *client, const ParseMessage &parsedMsg)
 
 void Server::handleCapCommand(Client *client, const std::vector<std::string> &params) 
 {
-    if (params.size() == 2 && params[0] == "LS" && params[1] == "302") {
+    if (params.size() == 2 && params[0] == "LS") {
 
 		client->conRegi[0] = true;
         client->serverReplies.push_back(":irssi CAP * LS :  \r\n");
